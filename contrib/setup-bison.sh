a142799790d84d0dba53e77b2c559afa0cea4029
tungche <zhang2000dc@gmail.com>
2024-06-13 12:25:14 +0800

    Update bison version

--- contrib/setup-bison.sh
+++ contrib/setup-bison.sh
@@ -12,17 +12,17 @@ if [ -d "$DEPS/bison" ]; then
     exit 0
 fi
 
-curl http://ftp.gnu.org/gnu/bison/bison-3.7.tar.xz --output $DEPS/bison-3.7.tar.xz
+curl http://ftp.gnu.org/gnu/bison/bison-3.8.tar.xz --output $DEPS/bison-3.8.tar.xz
 
-if [ ! -f "$DEPS/bison-3.7.tar.xz" ]; then
-    echo "It seems like downloading bison to $DEPS/bison-3.7.tar.xz failed"
+if [ ! -f "$DEPS/bison-3.8.tar.xz" ]; then
+    echo "It seems like downloading bison to $DEPS/bison-3.8.tar.xz failed"
     exit 1
 fi
 
 cd $DEPS
-tar -xf bison-3.7.tar.xz
-rm bison-3.7.tar.xz
-mv ./bison-3.7 ./bison
+tar -xf bison-3.8.tar.xz
+rm bison-3.8.tar.xz
+mv ./bison-3.8 ./bison
 cd bison
 mkdir bison-install
 ./configure --prefix $DEPS/bison/bison-install --exec-prefix $DEPS/bison/bison-install
